c46a7ca7ec402c58818d2a89752156f1bb9c5cee
Neha Dhaka <nehadhaka43@gmail.com>
2023-06-02 11:17:02 -0400

    initial commit on develop

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-fs.readFile("./data/videos.json", (err, data) => {
-  if (err) {
-    return console.log("Error reading file: ", err);
-  }
-  console.log(JSON.parse(data));
-});
 app.get("/", (req, res) => {
   res.send("welcome");
 });
